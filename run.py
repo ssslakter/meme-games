f8e5350210ceb813b29512043c14fcecb4a671ec
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2025-07-28 01:10:02 +0700

    add little tests

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from meme_games.main import serve
 
 if __name__ == '__main__':
-    serve(appname='meme_games.main', port=8000)
+    reload = os.environ.get('DEV', False)
+    serve(appname='meme_games.main', port=8000, reload=reload, reload_includes='meme_games/**' if reload else None)
